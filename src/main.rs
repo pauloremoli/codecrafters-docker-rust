e8bd053d85b318642ae4bac1263479c3cc6503b9
pauloremoli <paulo002@gmail.com>
2023-03-19 19:41:02 +0100

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use anyhow::{Context, Result};
+use std::process::{Command, Stdio};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+    Command::new(command)
         .args(command_args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .output()
         .with_context(|| {
             format!(
@@ -14,13 +17,6 @@ fn main() -> Result<()> {
                 command, command_args
             )
         })?;
-    
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
-        std::process::exit(1);
-    }
 
     Ok(())
 }
