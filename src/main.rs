6d1a420feb6561525d613ba967a5df0f029d223c
pauloremoli <paulo002@gmail.com>
2023-03-20 23:34:25 +0100

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use anyhow::{Context, Result};
 use std::process::{Command, Stdio};
+use std::os::unix::fs;
+use std::fs::File;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
+
+    create_sandbox()?;
+
+    
     let output = Command::new(command)
         .args(command_args)
         .stdout(Stdio::inherit())
@@ -26,5 +32,13 @@ fn main() -> Result<()> {
             }
         }
 
+    Ok(())
+}
+
+fn create_sandbox() -> Result<(), _> {
+    std::fs::create_dir("sandbox")?;
+    fs::chroot("/sandbox")?;
+    std::env::set_current_dir("/")?;
+    File::create("/dev/null")?;
     Ok(())
 }
